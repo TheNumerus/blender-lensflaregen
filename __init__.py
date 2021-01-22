0226a17ce4cf12f5dbafdfa661d5a9313bfd0116
Petr Volf <petr.volf216@gmail.com>
2021-01-22 17:25:11 +0100

    rendering into image

--- __init__.py
+++ __init__.py
@@ -1,21 +1,25 @@
 import bpy
 import numpy as np
-import math
+from . import image_processing
+
+if "bpy" in locals():
+    import importlib
+    importlib.reload(image_processing)
 
 bl_info = {
     "name": "Lens Flare Generator",
     "description": "Generates lens flare effects from image",
-    "blender": (2, 90, 0),
+    "blender": (2, 91, 0),
     "category": "Node",
     "author": "Petr Volf",
     "version": (0,1,0),
-    "warning": "NEFUNGUJE",
+    "warning": "Work in Progress version",
 }
 
 
-class LensFlarePanel(bpy.types.Panel):
-    bl_label = "Lens Flare Generators"
-    bl_idname = "LensFlarePanel"
+class LensFlareMainSettingsPanel(bpy.types.Panel):
+    bl_label = "Lens Flare Settings"
+    bl_idname = "LensFlareMainSettingsPanel"
     bl_space_type = 'NODE_EDITOR'
     bl_region_type = 'UI'
     bl_category = 'Lens Flares'
@@ -26,86 +30,125 @@ class LensFlarePanel(bpy.types.Panel):
         props = context.scene.lens_flare_props
  
         row = layout.row()
-        row.operator('node.lens_flare_operator')
+        row.prop(props, 'image')
+
         row = layout.row()
         row.operator('node.lens_flare_render')
-        row.prop(props, "invert")
 
+        row = layout.row()
+        row.prop(props, "posx")
+        row.prop(props, "posy")
 
-def create_flare_group(context, operator, group_name):
-    bpy.context.scene.use_nodes = True
-    
-    test_group = bpy.data.node_groups.new(group_name, 'CompositorNodeTree')
-    
-    group_in = test_group.nodes.new('NodeGroupInput')
-    group_in.location = (0, 0)
-    test_group.inputs.new('NodeSocketColor', 'Image Input')
-    test_group.inputs.new('NodeSocketFloatPercentage', 'Position X')
-    test_group.inputs.new('NodeSocketFloatPercentage', 'Position Y')
+class LensFlarePanel(bpy.types.Panel):
+    bl_label = "Flare"
+    bl_idname = "LensFlarePanel"
+    bl_space_type = 'NODE_EDITOR'
+    bl_region_type = 'UI'
+    bl_category = 'Lens Flares'
+    bl_parent_id = "LensFlareMainSettingsPanel"
 
-    ellipse = test_group.nodes.new('CompositorNodeEllipseMask')
-    ellipse.location = (200, 0)
+    def draw(self, context):
+        layout = self.layout
 
-    group_out = test_group.nodes.new('NodeGroupOutput')
-    group_out.location = (400, 0)
-    test_group.outputs.new('NodeSocketColor', 'Image Output')
+        props = context.scene.lens_flare_props
 
-    print(bpy.data.images.keys())
+        row = layout.row()
+        row.prop(props, 'flare_color')
+        row = layout.row()
+        row.prop(props, 'flare_size')
 
-    link = test_group.links.new
-    
-    link(group_in.outputs[0], ellipse.inputs[0])
-    link(ellipse.outputs[0], group_out.inputs[0])
-    
-    return test_group
+class LensFlareGhostsPanel(bpy.types.Panel):
+    bl_label = "Ghosts"
+    bl_idname = "LensFlareGhostsPanel"
+    bl_space_type = 'NODE_EDITOR'
+    bl_region_type = 'UI'
+    bl_category = 'Lens Flares'
+    bl_parent_id = "LensFlareMainSettingsPanel"
 
+    def draw(self, context):
+        layout = self.layout
 
-class AddLensFlareGroupOperator(bpy.types.Operator):
-    bl_label = "Add Lens Flare Node Group"
-    bl_idname = "node.lens_flare_operator"
-    
-    def execute(self, context):
-        custom_node_name = "Lens Flare Generator"
-        my_group = create_flare_group(self, context, custom_node_name)
-        test_node = context.scene.node_tree.nodes.new('CompositorNodeGroup')
-        test_node.node_tree = bpy.data.node_groups[my_group.name]
-        
-        return {'FINISHED'}
+        props = context.scene.lens_flare_props
+
+        row = layout.row()
+        row.operator('node.add_ghost')
+
+        row = layout.row()
+        row.prop(props, 'ghosts')
 
 
 class RenderLensFlareOperator(bpy.types.Operator):
     bl_label = "Render Lens Flare"
     bl_idname = "node.lens_flare_render"
+    bl_description = "Renders lens flare into selected image"
 
     def execute(self, context):
-        image = bpy.data.images['Viewer Node']
-        max_x, max_y = image.size
+        props: LensFlareProperties = context.scene.lens_flare_props
 
-        buffer = np.array(image.pixels)
+        # don't render if the is no valid output
+        if props.image is None:
+            self.report({'ERROR_INVALID_INPUT'}, "No image selected")
+            return {'CANCELLED'}
+        max_x, max_y = props.image.size
+
+        buffer = np.array(props.image.pixels)
         res_buffer = np.reshape(buffer, (-1, 4))
 
+        color = props.flare_color
 
-        inv = context.scene.lens_flare_props.invert
+        ratio = max_x / max_y
 
-        for pixel in res_buffer:
-            pixel[0] = math.modf(pixel[0] * inv)[0]
+        for i, pixel in enumerate(res_buffer):
+            x = i % max_x
+            y = i // max_x
+            value = image_processing.compute_flare_intensity(x, y, max_x, max_y, props, ratio)
+            pixel[0] = color[0] * value
+            pixel[1] = color[1] * value
+            pixel[2] = color[2] * value
 
         buffer = np.reshape(res_buffer, (-1))
 
-        if 'LensFlareRender' in bpy.data.images.keys():
-            output = bpy.data.images['LensFlareRender']
-        else:
-            output = bpy.data.images.new('LensFlareRender', max_x, max_y, alpha=True, float_buffer=True)
-        output.pixels = list(buffer)
+        props.image.pixels = list(buffer)
 
         return {'FINISHED'}
 
+class AddGhostOperator(bpy.types.Operator):
+    bl_label = "Create new ghost"
+    bl_idname = "node.add_ghost"
+    bl_description = "Creates new ghost"
+
+    def execute(self, context):
+        props: LensFlareProperties = context.scene.lens_flare_props
+
+        props.ghosts.add()
+
+        return {'FINISHED'}
+
+class LensFlareGhostPropertyGroup(bpy.types.PropertyGroup):
+    offset: bpy.props.FloatProperty(name="Offset", description="Ghost offset", default=0.0)
+    color: bpy.props.FloatVectorProperty(name="Color", description="Ghost color", subtype='COLOR_GAMMA', default=[0.9, 0.9, 0.9, 1.0], size=4)
+    size: bpy.props.FloatProperty(name="Size", description="Ghost Size", default=0.5)
 
 class LensFlareProperties(bpy.types.PropertyGroup):
-    invert: bpy.props.FloatProperty(name="Red Inversion", description="Value of red channel inversion", default=1.0)
+    posx: bpy.props.FloatProperty(name="X", description="Position of light source on X axis", default=0.5)
+    posy: bpy.props.FloatProperty(name="Y", description="Position of light source on X axis", default=0.5)
+    image: bpy.props.PointerProperty(name="Image", type=bpy.types.Image, description="Image to render to")
+    flare_color: bpy.props.FloatVectorProperty(name="Flare Color", description="Color of the main flare", subtype='COLOR_GAMMA', default=[0.9, 0.9, 0.9, 1.0], size=4)
+    flare_size: bpy.props.FloatProperty(name="Flare Size", description="Flare size relative to image size", default=0.5)
+    ghosts: bpy.props.CollectionProperty(name="Ghosts", type=LensFlareGhostPropertyGroup)
 
-
-_classes = [LensFlareProperties, LensFlarePanel, AddLensFlareGroupOperator, RenderLensFlareOperator]
+_classes = [
+    # properties
+    LensFlareGhostPropertyGroup,
+    LensFlareProperties,
+    # panels
+    LensFlareMainSettingsPanel,
+    LensFlarePanel,
+    LensFlareGhostsPanel,
+    # operators
+    RenderLensFlareOperator,
+    AddGhostOperator
+]
 
 def register():
     for cls in _classes:
