1c17aacf44ac758aef7db1f9566b271b136850a1
Petr.volf <petr.volf216@gmail.com>
2021-01-27 10:23:10 +0100

    ui additions, basic ghost rendering

--- __init__.py
+++ __init__.py
@@ -17,9 +17,9 @@ bl_info = {
 }
 
 
-class LensFlareMainSettingsPanel(bpy.types.Panel):
+class MainSettingsPanel(bpy.types.Panel):
     bl_label = "Lens Flare Settings"
-    bl_idname = "LensFlareMainSettingsPanel"
+    bl_idname = "LF_PT_MainSettings"
     bl_space_type = 'NODE_EDITOR'
     bl_region_type = 'UI'
     bl_category = 'Lens Flares'
@@ -39,31 +39,34 @@ class LensFlareMainSettingsPanel(bpy.types.Panel):
         row.prop(props, "posx")
         row.prop(props, "posy")
 
-class LensFlarePanel(bpy.types.Panel):
+class FlareSettingsPanel(bpy.types.Panel):
     bl_label = "Flare"
-    bl_idname = "LensFlarePanel"
+    bl_idname = "LF_PT_FlareSettings"
     bl_space_type = 'NODE_EDITOR'
     bl_region_type = 'UI'
     bl_category = 'Lens Flares'
-    bl_parent_id = "LensFlareMainSettingsPanel"
+    bl_parent_id = "LF_PT_MainSettings"
 
     def draw(self, context):
         layout = self.layout
+        layout.use_property_split = True
 
         props = context.scene.lens_flare_props
 
-        row = layout.row()
-        row.prop(props, 'flare_color')
-        row = layout.row()
-        row.prop(props, 'flare_size')
+        col = layout.column(align=True)
+        col.prop(props, 'flare_color', text='Flare Color')
+        col = layout.column(align=True)
+        col.prop(props, 'flare_size', text='Flare Size')
 
-class LensFlareGhostsPanel(bpy.types.Panel):
+class GhostsPanel(bpy.types.Panel):
     bl_label = "Ghosts"
-    bl_idname = "LensFlareGhostsPanel"
+    bl_idname = "LF_PT_Ghosts"
     bl_space_type = 'NODE_EDITOR'
     bl_region_type = 'UI'
     bl_category = 'Lens Flares'
-    bl_parent_id = "LensFlareMainSettingsPanel"
+    bl_parent_id = "LF_PT_MainSettings"
+
+    active_index = 0
 
     def draw(self, context):
         layout = self.layout
@@ -74,7 +77,43 @@ class LensFlareGhostsPanel(bpy.types.Panel):
         row.operator('node.add_ghost')
 
         row = layout.row()
-        row.prop(props, 'ghosts')
+
+        for ghost in props.ghosts:
+            box = layout.box()
+            row = box.row(align=True)
+
+            row.prop(ghost, 'offset')
+            row.prop(ghost, 'color', text='')
+            row.prop(ghost, 'size')
+            row.operator('object.select_all', text='', icon='X')
+
+class CameraOverridePanel(bpy.types.Panel):
+    bl_label = "Camera Override"
+    bl_idname = "LF_PT_CameraOverride"
+    bl_space_type = 'NODE_EDITOR'
+    bl_region_type = 'UI'
+    bl_category = 'Lens Flares'
+    bl_parent_id = "LF_PT_MainSettings"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw_header(self, context):
+        layout = self.layout
+        layout.prop(context.scene.lens_flare_props, 'use_override', text='')
+
+    @classmethod
+    def poll(cls, context):
+        return (context.object is not None)
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+        props = context.scene.lens_flare_props
+        layout.enabled = props.use_override
+
+        col = layout.column(align=True)
+        col.prop(props, 'blades', text='Aperture Blades')
+        col = layout.column(align=True)
+        col.prop(props, 'rotation', text='Rotation')
 
 
 class RenderLensFlareOperator(bpy.types.Operator):
@@ -95,8 +134,13 @@ class RenderLensFlareOperator(bpy.types.Operator):
         res_buffer = np.reshape(buffer, (-1, 4))
 
         color = props.flare_color
+        center_x = max_x / 2
+        center_y = max_y / 2
 
         ratio = max_x / max_y
+
+        flare_x = props.posx * max_x
+        flare_y = props.posy * max_y
 
         for i, pixel in enumerate(res_buffer):
             x = i % max_x
@@ -106,11 +150,26 @@ class RenderLensFlareOperator(bpy.types.Operator):
             pixel[1] = color[1] * value
             pixel[2] = color[2] * value
 
+            for ghost in props.ghosts:
+                ghost_x = center_x + (flare_x - center_x) * ghost.offset
+                ghost_y = center_y + (flare_y - center_y) * ghost.offset
+
+                value = min(max((ghost.size * 100) - ((x - ghost_x) ** 2 + (y - ghost_y) ** 2), 0.0), 1.0)
+
+                pixel[0] += ghost.color[0] * value
+                pixel[1] += ghost.color[1] * value
+                pixel[2] += ghost.color[2] * value
+
         buffer = np.reshape(res_buffer, (-1))
 
         props.image.pixels = list(buffer)
+        props.image.update()
+
+        # force compositing refresh
+        bpy.ops.render.render(write_still=True)
 
         return {'FINISHED'}
+
 
 class AddGhostOperator(bpy.types.Operator):
     bl_label = "Create new ghost"
@@ -124,31 +183,39 @@ class AddGhostOperator(bpy.types.Operator):
 
         return {'FINISHED'}
 
+
 class LensFlareGhostPropertyGroup(bpy.types.PropertyGroup):
     offset: bpy.props.FloatProperty(name="Offset", description="Ghost offset", default=0.0)
     color: bpy.props.FloatVectorProperty(name="Color", description="Ghost color", subtype='COLOR_GAMMA', default=[0.9, 0.9, 0.9, 1.0], size=4)
     size: bpy.props.FloatProperty(name="Size", description="Ghost Size", default=0.5)
 
+
 class LensFlareProperties(bpy.types.PropertyGroup):
     posx: bpy.props.FloatProperty(name="X", description="Position of light source on X axis", default=0.5)
     posy: bpy.props.FloatProperty(name="Y", description="Position of light source on X axis", default=0.5)
     image: bpy.props.PointerProperty(name="Image", type=bpy.types.Image, description="Image to render to")
+    use_override: bpy.props.BoolProperty(name="Camera Override", description="Use custom camera properties", default=False)
+    blades: bpy.props.IntProperty(name="Aperture Blades", description="Number of blades in aperture for polygonal bokeh (at least 3)", default=6, min=3)
+    rotation: bpy.props.FloatProperty(name="Aperture Rotation", description="Rotation of blades in aperture", default=0, subtype='ANGLE', unit='ROTATION')
     flare_color: bpy.props.FloatVectorProperty(name="Flare Color", description="Color of the main flare", subtype='COLOR_GAMMA', default=[0.9, 0.9, 0.9, 1.0], size=4)
     flare_size: bpy.props.FloatProperty(name="Flare Size", description="Flare size relative to image size", default=0.5)
     ghosts: bpy.props.CollectionProperty(name="Ghosts", type=LensFlareGhostPropertyGroup)
+
 
 _classes = [
     # properties
     LensFlareGhostPropertyGroup,
     LensFlareProperties,
     # panels
-    LensFlareMainSettingsPanel,
-    LensFlarePanel,
-    LensFlareGhostsPanel,
+    MainSettingsPanel,
+    FlareSettingsPanel,
+    GhostsPanel,
+    CameraOverridePanel,
     # operators
     RenderLensFlareOperator,
     AddGhostOperator
 ]
+
 
 def register():
     for cls in _classes:
--- image_processing.py
+++ image_processing.py
@@ -4,5 +4,5 @@ def compute_flare_intensity(x, y, max_x, max_y, props, ratio):
     delta_x = (x / max_x) - props.posx
     delta_y = (y / max_y) - props.posy
 
-    dist = math.sqrt((delta_x  ** 2) + (delta_y ** 2) / ratio)
+    dist = math.sqrt((delta_x ** 2) + (delta_y ** 2) / ratio)
     return max(props.flare_size - dist, 0.0)
