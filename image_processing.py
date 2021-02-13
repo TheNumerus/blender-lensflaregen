9d26c390b21c99126099986a1d7acf6789be0c6c
Petr Volf <petr.volf216@gmail.com>
2021-02-13 14:49:41 +0100

    moved debug cross

--- image_processing.py
+++ image_processing.py
@@ -7,3 +7,30 @@ def compute_flare_intensity(x, y, max_x, max_y, props, ratio):
 
     dist = math.sqrt((delta_x ** 2) + ((delta_y / ratio) ** 2))
     return max(props.flare_size - dist, 0.0)
+
+
+def draw_debug_cross(props):
+    cross_color = [2.0, 0.0, 2.0, 1.0]
+    thickness = 1
+    length = 10
+
+    max_x, max_y = props.image.size
+
+    center_x = int(max_x * props.posx)
+    center_y = int(max_y * props.posy)
+
+    buffer = [0.0 for x in range(max_x * max_y * 4)]
+
+    # draw horizontal line
+    for y in range(max(center_y - thickness, 0), min(center_y + thickness, max_y)):
+        for x in range(max(center_x - length, 0), min(center_x + length, max_x)):
+            index = (x + y * max_x) * 4
+            buffer[index:index+4] = cross_color
+
+    # draw vertical line
+    for y in range(max(center_y - length, 0), min(center_y + length, max_y)):
+        for x in range(max(center_x - thickness, 0), min(center_x + thickness, max_x)):
+            index = (x + y * max_x) * 4
+            buffer[index:index+4] = cross_color
+
+    props.image.pixels[:] = buffer
--- operators.py
+++ operators.py
@@ -59,32 +59,6 @@ class RenderLensFlareOperator(bpy.types.Operator):
 
         return props.blades, props.rotation
 
-    def draw_debug_cross(self, props):
-        cross_color = [2.0, 0.0, 2.0, 1.0]
-        thickness = 1
-        length = 10
-
-        max_x, max_y = props.image.size
-
-        center_x = int(max_x * props.posx)
-        center_y = int(max_y * props.posy)
-
-        buffer = [0.0 for x in range(max_x * max_y * 4)]
-
-        # draw horizontal line
-        for y in range(max(center_y - thickness, 0), min(center_y + thickness, max_y)):
-            for x in range(max(center_x - length, 0), min(center_x + length, max_x)):
-                index = (x + y * max_x) * 4
-                buffer[index:index+4] = cross_color
-
-        # draw vertical line
-        for y in range(max(center_y - length, 0), min(center_y + length, max_y)):
-            for x in range(max(center_x - thickness, 0), min(center_x + thickness, max_x)):
-                index = (x + y * max_x) * 4
-                buffer[index:index+4] = cross_color
-
-        props.image.pixels[:] = buffer
-
     def execute(self, context):
         props: LensFlareProperties = context.scene.lens_flare_props
 
@@ -102,7 +76,7 @@ class RenderLensFlareOperator(bpy.types.Operator):
             return {'CANCELLED'}
 
         if props.debug_pos:
-            self.draw_debug_cross(props)
+            image_processing.draw_debug_cross(props)
         else:
             # TODO move somewhere else
             max_x, max_y = props.image.size
